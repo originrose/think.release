6c9a0b89b650941ddcd27bea1cb4b4e79f832212
Chris Nuernberger <chris@thinktopic.com>
2017-03-22 17:45:38 -0600

    Finishing docker build script.

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -9,4 +9,4 @@ lein clean && lein uberjar
 echo "Building docker.thinktopic.com/think.release:$DOCKER_VERSION"
 
 docker build -t docker.thinktopic.com/think.release:$DOCKER_VERSION .
-#docker push docker.thinktopic.com/think.release:$DOCKER_VERSION
+docker push docker.thinktopic.com/think.release:$DOCKER_VERSION
