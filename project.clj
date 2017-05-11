11c2fcc279e55141037e540d085d297c93932eb1
Chris Nuernberger <chris@thinktopic.com>
2017-05-11 10:19:51 -0600

    Bump config version due to major bugfix.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/think.config "0.2.4"]
+                 [thinktopic/think.config "0.3.0"]
                  [org.clojure/tools.cli "0.3.5"]]
 
   :main         think.release.main
