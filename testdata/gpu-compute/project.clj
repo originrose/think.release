4d537f9b94bade45a2588746d6186e96ece6e26a
Chris Nuernberger <chris@thinktopic.com>
2017-05-11 14:12:03 -0600

    Removing files that aren't used at the moment.

--- testdata/gpu-compute/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject thinktopic/gpu-compute "0.1.0-SNAPSHOT"
-  :description "Gpu backend for thinktopic's compute library"
-  :url "http://thinktopic.com/cortex"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
-                 [thinktopic/compute ""0.1.0-2017-03-22-02-09""]])
